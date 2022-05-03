4440b5a6d81f8c68d2aea78b1c8ee20be4a2f173
DRMus <78694252+DRMus@users.noreply.github.com>
2022-05-03 12:49:56 +0400

    Создан поиск города на карте и сохрание изображения на жесткий диск

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
+import re
+
 import requests
 
-resp = requests.get('https://static-maps.yandex.ru/1.x/?ll=37.677751,55.757718&spn=0.016457,0.00619&l=map')
+query = input('Введите город: ')
 
-print(resp.status_code)
+host = 'https://geocode-maps.yandex.ru/1.x/'
+
+params_query_get = {
+    'apikey': '1a78dda4-1157-4234-8cfa-8624cb129414',
+    'geocode': query,
+    'format': 'json'
+}
+resp = requests.get(host, params=params_query_get)
+response = resp.json()
+
+resp_static = requests.get('https://static-maps.yandex.ru/1.x/?ll=' +
+                           re.sub(r"\s", r",",
+                                  response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["Point"][
+                                      "pos"])
+                           + '&spn=0.016457,0.00619&l=map')
+
+print(resp_static.status_code)
 
 with open('map.png', 'wb') as f:
-    f.write(resp.content)
+    f.write(resp_static.content)
